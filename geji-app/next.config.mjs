20e785b0461182be01f93c6e9e0486545a081453
messele <messele@tukulllc.com>
2024-06-08 14:06:54 -0400

    Some changes for github pages

--- geji-app/next.config.mjs
+++ geji-app/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "geji-app", // needed to set build directory for Github pages
+    output: "export", // needed to create static bundle for Github pages
+    reactStrictMode: true,
+};
 
 export default nextConfig;
