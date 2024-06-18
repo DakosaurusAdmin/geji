6ceb9ac215868eb8122b1823bb3a5265ae50eb9d
messele <messele@tukulllc.com>
2024-06-18 02:58:30 -0400

    Fixing github standalone deployment - unoptimizing image

--- geji-app/next.config.mjs
+++ geji-app/next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
     output: process.env['GITHUB_ACTIONS'] ? "export": undefined, // needed to create static bundle for Github pages
     reactStrictMode: true,
-    assetPrefix: process.env['GITHUB_ACTIONS'] ? '/geji/' : undefined
+    assetPrefix: process.env['GITHUB_ACTIONS'] ? '/geji/' : undefined,
+
+    images: {
+        unoptimized: process.env['GITHUB_ACTIONS'] ? true : false
+      }
 };
 
 export default nextConfig;
