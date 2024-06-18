b87684df3c83230147b606ef62e7456de4445b58
messele <messele@tukulllc.com>
2024-06-18 02:53:10 -0400

    Fixing github standalone deployment

--- geji-app/next.config.mjs
+++ geji-app/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   // output: "export", // needed to create static bundle for Github pages
+    output: process.env['github.workspace'] ? "export": undefined, // needed to create static bundle for Github pages
     reactStrictMode: true,
-    // assetPrefix: process.env.NODE_ENV === 'production'? '/geji/' : undefined
+    assetPrefix: process.env['github.workspace'] ? '/geji/' : undefined
 };
 
 export default nextConfig;
